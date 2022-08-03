5cd7c96ee0340c9d1ba7cd28e905f49043e335c2
Slish Vasiliy <white-aftor@yandex.ru>
2022-08-04 00:57:07 +0300

    update dependency - fix incompatibility

--- src/components/functionalBar/FunctionalBar.tsx
+++ src/components/functionalBar/FunctionalBar.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import ButtonSYS from '../UI/buttonSYS/ButtonSYS'
 import classes from './functional.module.scss'
 
 const FunctionalBar = () => {
-  return <div className={classes.functionalBar}></div>
+  return (
+    <div className={classes.functionalBar}>
+      <ButtonSYS active={true}>Создать заказ</ButtonSYS>
+      <ButtonSYS>Принять запчасть</ButtonSYS>
+    </div>
+  )
 }
 
 export default FunctionalBar
